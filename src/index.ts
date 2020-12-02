98474cda309d53f4375a24256e0d1b6b05c8ebd6
brianebill <lumeo@me.com>
2020-12-02 12:08:32 -0500

    add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { importEsm } from './import';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv)).argv;
 
-importEsm();
+const args = yargs.options({
+  subject: { type: 'string', demandOption: true, alias: 's' },
+}).argv;
+
+console.log(argv);
